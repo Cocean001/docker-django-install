4aa1d0fe3fe53069833f2da55d50259dfaee03d5
cocean <60824693+Cocean001@users.noreply.github.com>
2021-08-11 15:11:23 +0800

    Update django-install.sh

--- django-install.sh
+++ django-install.sh
@@ -40,7 +40,7 @@ echo "export VISIBLE=now" >> /etc/profile
 green "sshserver配置写入完成"
 
 echo "正在安装环境依赖"
-sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install default-libmysqlclient-dev -y
 sudo apt install libssl-dev -y
 sudo apt-get install default-libmysqlclient-dev -y
 sudo apt install libcrypto++-dev -y
